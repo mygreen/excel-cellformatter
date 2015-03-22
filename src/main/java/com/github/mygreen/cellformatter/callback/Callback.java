5b54f02a6e60858ab16d98e15c78817938ce5596
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2015-03-22 12:58:02 +0900

    Javadocの修正

--- src/main/java/com/github/mygreen/cellformatter/callback/Callback.java
+++ src/main/java/com/github/mygreen/cellformatter/callback/Callback.java
@@ -14,6 +14,7 @@ public interface Callback<T> {
     /**
      * フォーマットの後に実行する処理。
      * @param data 変換元のデータ
+     * @param value フォーマットされた値。
      * @return 処理後の値
      */
     String call(T data, String value);
