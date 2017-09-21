18c43e156bce7fee7f5f3b814ab6b7c413fc6b63
xaatw0 <xaatw0@gmail.com>
2017-09-21 11:39:15 +0900

    セルの書式設定が「!¥ 0;- !¥ 0;!¥ 0;''」の場合、例外が発生するので、セクションが4つの場合の処理を追加した

--- src/test/java/com/github/mygreen/cellformatter/CustomFormatterFactoryTest.java
+++ src/test/java/com/github/mygreen/cellformatter/CustomFormatterFactoryTest.java
@@ -14,76 +14,76 @@ import com.github.mygreen.cellformatter.lang.MSColor;
  *
  */
 public class CustomFormatterFactoryTest {
-    
+
     // エスケープ文字のテスト
     @Test
     public void testEscapedChar() {
-        
+
         CommonCell testCell = new TestNumberCell(23.1, (short)0, "*a!a!a##.00");
         CustomFormatterFactory factory = new CustomFormatterFactory();
         CustomFormatter formatter = factory.create(testCell.getFormatPattern());
-        
+
         CellFormatResult actual = formatter.format(testCell);
         assertThat(actual.getText(), is("aa23.10"));
     }
-    
+
     // エスケープ文字のテスト
     @Test
     public void testEscapedChar2() {
-        
+
         // "\"#,##0_);[Red]\("\"#,##0\)
         NumberCell<Integer> cell = new NumberCell<Integer>(-1234, "\"\\\"#,##0_);[Red]\\(\"\\\"#,##0\\)");
-        
+
         CustomFormatterFactory factory = new CustomFormatterFactory();
         CustomFormatter formatter = factory.create(cell.getFormatPattern());
-        
+
         CellFormatResult actual = formatter.format(cell);
         assertThat(actual.getText(), is("(\\1,234)"));
         assertThat(actual.getTextColor(), is(MSColor.RED));
     }
-    
+
     // セクションの個数のテスト
     @Test
     public void testSectionNumber() {
-        
+
         CustomFormatterFactory factory = new CustomFormatterFactory();
-        
+
         try {
             // 4つの場合
             factory.create("###1;###2;###3;4@");
-            
+            factory.create("!¥ 0;- !¥ 0;!¥ 0;''");
         } catch(Exception e) {
             e.printStackTrace();
             fail();
         }
-        
+
         try {
             // 5つの場合
             factory.create("###1;###2;###3;###4;5@");
             fail();
         } catch(Exception e) {
-            
+
             assertThat(e, instanceOf(CustomFormatterParseException.class));
         }
-        
+
     }
-    
+
     // フォーマット結果のテスト
     @Test
     public void testCellFormatResult() {
-        
+
         CommonCell testCell = new TestNumberCell(100.1, (short)0, "[赤][>100]\">\"##.0#;[青][=100]\"=\"##.0#;[紫]\"その他\"##.0#");
-        
+
         CustomFormatterFactory factory = new CustomFormatterFactory();
         CustomFormatter formatter = factory.create(testCell.getFormatPattern());
-        
+
         CellFormatResult actual = formatter.format(testCell);
-        
+
         assertThat(actual.getValueAsDoulbe(), is(100.1));
         assertThat(actual.getText(), is(">100.1"));
         assertThat(actual.getTextColor(), is(MSColor.RED));
         assertThat(actual.getSectionPattern(), is("[赤][>100]\">\"##.0#"));
         assertThat(actual.getCellType(), is(FormatCellType.Number));
-        
+
     }
 }
