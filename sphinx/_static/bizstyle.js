15357823ffa3613ddf37083fcb92fe76631daec4
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2017-09-22 00:46:55 +0900

    v0.9.2のサイトのコミット

--- sphinx/_static/bizstyle.js
+++ sphinx/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("Excel-CellFormatter 0.9.1 ドキュメント");
+        $("li.nav-item-0 a").text("Excel-CellFormatter 0.9.2 ドキュメント");
     }
 });
