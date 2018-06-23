bb1468d49b2dbfdcfa844707f024f8f5dedf1dc2
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2018-06-23 13:14:04 +0900

    テスト用にデバッグ追加

--- make_sphinx_html.sh
+++ make_sphinx_html.sh
@@ -6,17 +6,25 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
 if [ -e ./src/site/sphinx/build ]; then
+  echo "step1"
   /bin/rm -rf ./src/site/sphinx/build
 fi
 
+echo "step2"
 cd ./src/site/sphinx
+
+echo "step3"
 make html PACKAGE_VERSION=$1
+
+echo "step4"
 /usr/bin/chown -R jenkins:jenkins ./src/site/sphinx/build
 
 ## copy html dir
 cd $SCRIPT_DIR
+echo "step5"
 /bin/rm -rf ./target/site/sphinx
 /bin/mkdir -p ./target/site/sphinx
+echo "step6"
 /bin/cp -vr ./src/site/sphinx/build/html/* ./target/site/sphinx/
 
 ## github-pagesのsphinx対応
