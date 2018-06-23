2c194aa051701d3cec8d0af8a0d6db9ed4ecbfc8
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2018-06-23 13:33:39 +0900

    ステップのコメントを修正

--- make_sphinx_html.sh
+++ make_sphinx_html.sh
@@ -6,28 +6,29 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
 if [ -e ./src/site/sphinx/build ]; then
-  echo "step1"
+  echo "step - remove old build directory."
   sudo /bin/rm -rf ./src/site/sphinx/build
 fi
 
-echo "step2"
 cd ./src/site/sphinx
 
-echo "step3"
+echo "step - make html by sphinx."
 make html PACKAGE_VERSION=$1
 
-echo "step4"
+echo "step - change owner for build directry with jenkins."
 sudo /usr/bin/chown -R jenkins:jenkins ./build
 
 ## copy html dir
 cd $SCRIPT_DIR
 
-echo "step5"
+echo "step - remove target sphinx directory."
 sudo /bin/rm -rf ./target/site/sphinx
 /bin/mkdir -p ./target/site/sphinx
-echo "step6"
+
+echo "step - copy build html to target directory."
 /bin/cp -vr ./src/site/sphinx/build/html/* ./target/site/sphinx/
 
 ## github-pagesのsphinx対応
+echo "step - create file or .nojekyll."
 touch ./target/site/.nojekyll
 
