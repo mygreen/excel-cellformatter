08d8631e5d81569f2c4e2eec7f6b3fd896ee7069
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2018-06-24 23:06:16 +0900

    site 0.10

--- sphinx/_static/bizstyle.js
+++ sphinx/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("Excel-CellFormatter 0.9.2 ドキュメント");
+        $("li.nav-item-0 a").text("Excel-CellFormatter 0.10 ドキュメント");
     }
 });
