35e15fb90615545a71529b2f31752fcf50dfbe6b
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2019-04-01 23:42:48 +0900

    新元号「令和」の対応

--- src/test/java/com/github/mygreen/cellformatter/lang/EraResolverTest.java
+++ src/test/java/com/github/mygreen/cellformatter/lang/EraResolverTest.java
@@ -1,10 +1,8 @@
 package com.github.mygreen.cellformatter.lang;
 
 import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
-import java.sql.Timestamp;
 import java.util.Date;
 
 import org.junit.Test;
@@ -12,80 +10,80 @@ import org.junit.Test;
 /**
  * {@link EraResolver}のテスタ
  *
- * @version 0.9
+ * @version 0.11
  * @since 0.5
  * @author T.TSUCHIE
  *
  */
 public class EraResolverTest {
-    
+
     @Test
     public void testIsUnkown() {
-        
+
         EraResolver resolver = new EraResolver();
-        
+
         assertThat(resolver.getEra(MSLocale.US).isUnkndown(), is(true));
-        
+
         assertThat(resolver.getEra(MSLocale.JAPANESE).isUnkndown(), is(false));
-        
+
     }
-    
+
     @Test
     public void testPeriod() {
-        
+
         EraResolver resolver = new EraResolver();
-        
+
         Era era = resolver.getEra(MSLocale.JAPANESE);
-        
+
         {
             // 存在する期間
             Date date = ExcelDateUtils.parseDate("2015-01-01 00:00:00.000");
             assertThat(era.contains(date), is(true));
-            
+
             EraPeriod period = era.getTargetPeriod(date);
             assertThat(period.isUnknown(), is(false));
         }
-        
+
         {
             // 存在しない期間
             Date date = ExcelDateUtils.parseDate("1850-01-01 00:00:00.000");
             assertThat(era.contains(date), is(false));
-            
+
             EraPeriod period = era.getTargetPeriod(date);
             assertThat(period.isUnknown(), is(true));
         }
-        
+
     }
-    
+
     /**
      * ユーザのクラスパスのルートに配置している定義を読み込む
-     * <p>2018-12-31で平成が終わる定義</p>
+     * <p>2099-12-31で令和が終わる定義</p>
      */
     @Test
     public void testUserSetting() {
-        
+
         EraResolver resolver = new EraResolver();
-        
+
         Era era = resolver.getEra(MSLocale.JAPANESE);
-        
+
         {
-            // 平成の終わり
-            Date date = ExcelDateUtils.parseDate("2018-12-31 23:59:59.999");
+            // 令和の終わり
+            Date date = ExcelDateUtils.parseDate("2089-12-31 23:59:59.999");
             EraPeriod period = era.getTargetPeriod(date);
-            
-            assertThat(period.getName(), is("平成"));
+
+            assertThat(period.getName(), is("令和"));
             assertThat(period.getEndDate(), is(not(nullValue())));
-            
+
         }
-        
+
         {
-            // 平成の次の仮号の場合
-            Date date = ExcelDateUtils.parseDate("2019-01-01 00:00:00.000");
+            // 令和の次の仮号の場合
+            Date date = ExcelDateUtils.parseDate("2090-01-01 00:00:00.000");
             EraPeriod period = era.getTargetPeriod(date);
-            
+
             assertThat(period.getName(), is("仮号"));
             assertThat(period.getEndDate(), is(nullValue()));
         }
-        
+
     }
 }
