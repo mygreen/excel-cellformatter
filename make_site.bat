0bebb781ea43e4bc389d79a7dc9d59cd233e9ac2
tatsu-no-otoshigo <tatsuo.tsuchie@gmail.com>
2024-01-02 16:58:47 +0900

    ビルド用バッチをmaven wrapperに変更。

--- make_site.bat
+++ make_site.bat
@@ -5,11 +5,11 @@ cd %~p0
 
 call env.bat
 
-call mvn -version
+call mvnw -version
 
-call mvn clean
+call mvnw clean
 mkdir target
-call mvn site -Dgpg.skip=true -Dfile.encoding=UTF-8 > target/site.log 2>&1
+call mvnw site -Dgpg.skip=true -Dfile.encoding=UTF-8 > target/site.log 2>&1
 
 REM github-pages‚Ì‘Î‰
 echo "" > .\target\site\.nojekyll
